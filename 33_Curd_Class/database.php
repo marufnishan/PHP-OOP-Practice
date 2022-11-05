80dd98b11606a6a46f4d70b9a1da7b8ce52b9dd2
marufnishan <nasim15-2328@diu.edu.bd>
2022-11-05 13:40:59 +0600

    OOP Update Added

--- 33_Curd_Class/database.php
+++ 33_Curd_Class/database.php
@@ -73,13 +73,63 @@ class database{
     }
 	
 	//Function to delete table or row(s) in database
-	public function delete(){
-		
+	public function delete($table,$where= null){
+		if($this->tableExists($table)){
+			$sql = "DELETE FROM $table";
+			if($where !=null){
+                $sql.=" WHERE $where";
+            }
+			if($this->mysqli->query($sql)){
+                array_push($this->reasult,$this->mysqli->affected_rows);
+                return true;
+            }
+            else{
+                array_push($this->reasult,$this->mysqli->error);
+                return false;
+            }
+		}else{
+			return false;
+		}
 	}
 	
 	//Function to SELECT from the database
-	public function select(){
-		
+	public function select($table,$rows="*",$join = null,$where=null,$order=null,$limit=null){
+		if($this->tableExists($table)){
+			$sql = "SELECT $rows FROM $table";
+			if($join != null){
+				$sql .= " JOIN $join";
+			}
+			if($where != null){
+				$sql .= " WHERE $where";
+			}
+			if($order != null){
+				$sql .= " ORDER BY $order";
+			}
+			if($limit != null){
+				$sql .= " LIMIT 0, $limit";
+			}
+
+			$query = $this->mysqli->query($sql);
+			if($query){
+				$this->reasult = $query->fetch_all(MYSQLI_ASSOC);
+				return true;
+			}else{
+				array_push($this->reasult,$this->mysqli->error);
+				return false;
+			}
+		}else{
+			return false;
+		}
+	}
+	public function sql($sql){
+		$query = $this->mysqli->query($sql);
+		if($query){
+			$this->reasult = $query->fetch_all(MYSQLI_ASSOC);
+			return true;
+		}else{
+			array_push($this->reasult,$this->mysqli->error);
+			return false;
+		}
 	}
 
 
--- 33_Curd_Class/update.php
+++ 33_Curd_Class/update.php
@@ -2,7 +2,7 @@
 include 'database.php';
 
 $obj = new database();
-$obj->update('students',['name'=>'Tusar','age'=>'20','city'=>'Dhakha'],'id="3"');
+$obj->update('students',['age'=>'25','city'=>'Tongi'],'id="4"');
 echo "update result is :";
 print_r($obj->getReasult());
 ?>
